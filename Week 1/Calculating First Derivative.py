14d9a83abadf6be86d76c9a6118fdfd06f91d0c2
Karamel-King <sameehossain1379@gmail.com>
2022-07-27 13:52:27 +1000

    Update Calculating First Derivative.py
    Week 1 Tutorial

--- Week 1/Calculating First Derivative.py
+++ Week 1/Calculating First Derivative.py
@@ -6,10 +6,24 @@ Created on Wed Jul 27 13:38:50 2022
 """
 import numpy as np
 
+
 def forward_difference(f,x,h):
+    approx = (f(x+h)-f(x))/h
+    return approx
+
+def backward_difference(f,x,h):
+    approx = (f(x+h)-f(x))/h
+    return approx
+
+def central_difference(f,x,h):
     approx = (f(x+h)-f(x))/h
     return approx
 
 def func_a(x):
     return x**3
 
+def func_b(x):
+    return 3*x**2 - 2*x
+
+def func_c(x):
+    return np.sin(x)
